7f837d33a4cdaaec1c20a6200c7ac6474178a78c
DolipranZ <stephan.dupeu@gmail.com>
2021-01-12 17:08:21 +0100

    regex sur l'entree utilisateur

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -32,7 +32,7 @@ exports.deleteSauce = (req, res, next) => {
       const filename = sauce.imageUrl.split('/images/')[1];
       fs.unlink(`images/${filename}`, () => {
         Sauce.deleteOne({ _id: req.params.id })
-          .then(() => res.status(201).json({ message: 'Objet supprimé !'}))
+          .then(() => res.status(201).json({ message: 'Sauce supprimée !'}))
           .catch(error => res.status(400).json({ error }));
       });
     })
--- controllers/user.js
+++ controllers/user.js
@@ -4,17 +4,22 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-      const user = new User({
-        email: req.body.email,
-        password: hash
-      });
-      user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch(error => { console.log(error); res.status(400).json({ error })});
-    })
-    .catch(error => res.status(500).json({ error }));
+  let emailRegex = /^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]­{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$/;
+  if (emailRegex.test(req.body.email)) {
+    bcrypt.hash(req.body.password, 10)
+      .then(hash => {
+        const user = new User({
+          email: req.body.email,
+          password: hash
+        });
+        user.save()
+          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+          .catch(error => { console.log(error); res.status(400).json({ error })});
+      })
+      .catch(error => res.status(500).json({ error }));
+  } else {
+    res.status(400).json({ error });
+  }
 };
 
 exports.login = (req, res, next) => {
